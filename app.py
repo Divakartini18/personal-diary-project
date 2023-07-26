91956308124ac882357694ee5474299fb7e6bcc7
Divakartini <divakartini9@gmail.com>
2023-07-26 10:41:46 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.lkwpa0o.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URL = os.environ.get("MONGODB_URL")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URL)
+db = client[DB_NAME]
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/diary', methods=['GET'])
 def show_diary():
-    articles = list(db.diary.find({},{'_id':False}))
+    articles = list(db.diary.find({}, {'_id': False}))
     return jsonify({'articles': articles})
+
 
 @app.route('/diary', methods=['POST'])
 def save_diary():
@@ -36,13 +49,14 @@ def save_diary():
 
     doc = {
         'file': filename,
-        'profile':profilename,
-        'title':title_receive,
-        'content':content_receive
+        'profile': profilename,
+        'title': title_receive,
+        'content': content_receive
     }
     db.diary.insert_one(doc)
 
-    return jsonify({'msg':'Upload berhasil !'})
+    return jsonify({'msg': 'Upload berhasil !'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
